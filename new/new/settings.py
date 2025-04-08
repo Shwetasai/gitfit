fe19df9fa9d3e69c82e8da50a76d51c8dab493c6
shwetasai <shwetasaini250796@gmail.com>
2025-04-08 16:19:00 +0530

    third commit

--- new/new/settings.py
+++ new/new/settings.py
@@ -3,12 +3,6 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-3ne_tqq8&dq3px*b8jdqkd&0=%72v_7d#9bb^+a)mov!*$eke2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
